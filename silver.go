b3a4b832579eb79443c1ab7df88b32949eab71a7
Brandon Strickland <Brandon@ShaleApps.com>
2015-04-18 13:36:33 -0400

    simplifying for additions

--- silver.go
+++ silver.go
@@ -7,8 +7,7 @@
 package main
 
 import (
-	"errors"
-	"flag"
+//	"flag"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -21,7 +20,7 @@ import (
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 // i dont dont know what this is for.
-var addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
+//var addr = flag.Bool("addr", false, "find open address and print to final-port.txt")
 
 // Page is the contents of the page the browser will display. A page
 // is broken into two pieces, the title and the body of the page.
@@ -29,12 +28,6 @@ type Page struct {
 	Title string
 	Body  []byte // We are using byte slices since the libraries
 	// will be expecting byte slices.
-}
-
-// Save takes the page and converts it into a .txt for storage in the HDD.
-func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 // loadPage reads from the HDD and returns the resulting page back to the
@@ -63,60 +56,32 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
-// viewHandler loads the page requested from the user via URL and returns html
-// for the user.
-func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-		return
-	}
-	renderTemplate(w, "view", p)
-}
-
-// editHandler loads the html for editting a page and returns the user to
-// to the edit page.
-func editHandler(w http.ResponseWriter, r *http.Request, title string) {
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	renderTemplate(w, "edit", p)
-}
-
-// saveHandler takes form data and sends the Page to the save function to write
-// it to the HDD.
-func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
-	body := r.FormValue("body")
-	p := &Page{Title: title, Body: []byte(body)}
-	err = p.save()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	http.Redirect(w, r, "/view/"+title, http.StatusFound)
-}
-
 // makeHandler passes a http.Handler back to HandleFunc. This is allows us to more
 // or less cut down on error handling duplication done for things like checking
 // the title. Now, we simply pass the title to the function with this.
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		m := validPath.FindStringSubmatch(r.ULR.Path)
-		if m != nill {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m != nil {
 			http.NotFound(w, r)
 			return
 		}
-		fn(w, r, m[2])
+		fn(w, r, m[0])
 	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: "home", Body: []byte("the page didn't load correctly")}
+	}
+	renderTemplate(w, "home", p)
 }
 
 // main sends patterns to the handler and then we let the ListenAndServe take
 // over and we block on it until the application crashes or is stopped by someone
 // who started it.
 func main() {
-	flag.Parse()
-	http.HandleFunc("/view/", makeHandler(viewHandler))
-	http.HandleFunc("/save/", makeHandler(saveHandler))
-	http.HandleFunc("/edit/", makeHandler(editHandler))
-
+	http.HandleFunc("/", makeHandler(homeHandler))
+	http.ListenAndServe(":8080", nil)
 }
