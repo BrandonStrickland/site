28adee252b9071e565ea2f9c3d6965dad2d8ff62
Brandon Strickland <Brandon@ShaleApps.com>
2015-05-13 13:31:11 -0400

    added test stubs, one test partially written, test fails, needs debugging

--- gold.go
+++ gold.go
@@ -20,7 +20,7 @@ type Point struct {
 }
 
 // initTable creates a table for us to use.
-func initTable(db *sql.DB) error {
+func InitTable(db *sql.DB) error {
 	_, err := db.Exec(
 		"create table `temp` ( `ID` bigint(20) NOT NULL, `X` bigint(20) NOT NULL, `Y` bigint(20) NOT NULL, PRIMARY KEY (`ID`) ) ENGINE=MEMORY;")
 	if err != nil {
@@ -32,7 +32,7 @@ func initTable(db *sql.DB) error {
 // fillTable takes the db pointer and a point and adds to to the database
 // If it doesn't add it to the table, for this application, it doesn't matter.
 // So we aren't going to deal with the error.
-func fillTable(db *sql.DB, point Point) error {
+func FillTable(db *sql.DB, point Point) error {
 	_, err := db.Exec("insert into temp(id,x,y) values(?,?,?)", point.ID, point.X, point.Y)
 	if err != nil {
 		return err
@@ -42,7 +42,7 @@ func fillTable(db *sql.DB, point Point) error {
 
 // cleanTables ensures we aren't playing with any old values. restarting the server
 // for each run would be silly.
-func cleanTable(db *sql.DB) error {
+func CleanTable(db *sql.DB) error {
 	_, err := db.Exec("drop table if exists `temp`")
 	if err != nil {
 		return err
@@ -51,47 +51,47 @@ func cleanTable(db *sql.DB) error {
 }
 
 // jsonHandler takes a byte slice from the readAll call and sends it to function that inserts it into the table.
-func jsonHandler(db *sql.DB, body []byte) error {
+func JsonHandler(db *sql.DB, body []byte) error {
 	var p Point
 	err := json.Unmarshal(body, &p)
 	if err != nil {
 		return err
 	}
 
-	err = fillTable(db, p)
+	err = FillTable(db, p)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func setupDatabase() (*sql.DB, error) {
+func SetupDatabase() (*sql.DB, error) {
 	database, err := sql.Open("mysql", "brandon:password@tcp(127.0.0.1:3306)/test")
 	if err != nil {
 		return nil, err
 	}
-	
+
 	err = database.Ping()
 	if err != nil {
 		return nil, err
 	}
 
-	if err = cleanTable(database); err != nil {
+	if err = CleanTable(database); err != nil {
 		return nil, err
 	}
-	if err = initTable(database); err != nil {
+	if err = InitTable(database); err != nil {
 		return nil, err
 	}
 	return database, err
 }
 
-func handler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+func Handler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	r.ParseForm()
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, "something happened", http.StatusInternalServerError)
 	}
-	if err = jsonHandler(db, body); err != nil {
+	if err = JsonHandler(db, body); err != nil {
 		http.Error(w, "something happened", http.StatusInternalServerError)
 	}
 }
@@ -101,26 +101,24 @@ func handler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 // secure way to open a connection without showing it in the code. Probably passing them as
 // arguments to the program when it runs. can use os.Args() for this I think.
 //
-// ~~~~~~things to fix~~~~~~~// THE GLOBAL DB POINTER
-// Ideally, there should be one entry and one exit, reconstruct functions to correctly handle errors.
-//
 // The program idea follows:
 // 1. listenAndServe
 // 2. receive json and unmarshal it
 // 3. send the point to the mysql database
+// 4. send ok if successful or error if not
 //
 // next to implement:
 // 1. add auth tokens
 // 2. work more with internet response codes
 func main() {
-	db, err := setupDatabase()
+	db, err := SetupDatabase()
 	if err != nil {
 		log.Fatal("The database could not be correctly configured")
 	}
 	defer db.Close()
-	
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handler(w,r,db)
+		Handler(w, r, db)
 	})
 	http.ListenAndServe(":8080", nil)
 }
--- gold_test.go
+++ gold_test.go
@@ -2,12 +2,89 @@ package main
 
 import (
 	"testing"
-	"log"
-	"fmt"
 	"net/http"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"bytes"
+	//"net/http"
 	"net/http/httptest"
+	"database/sql"
+	_ "github.com/erikstmartin/go-testdb"
 )
 
-func main() {
+// I don't feel too terrible for making this global in a test.
+//var db,_ = sql.Open("testdb", "")
+var db,_ = sql.Open("mysql", "brandon:password@tcp(127.0.0.1:3306)/test")
+
+/*
+type Point struct {
+	ID int
+	X  int
+	Y  int
+}
+
+func TestInitTable(t *testing.T) {
 	
+}
+
+func TestFillTable(t *testing.T) {
+
+}
+
+func TestCleanTable(t *testing.T) {
+
+}
+
+func TestJsonHandler(t *testing.T) {
+
+}
+
+func TestSetupDatabase(t *tsting.T) {
+
+}
+*/
+
+// generatePoint takes in an id so that when we add it to the database,
+// we can have the primary key be the id and since it is incremented,
+// inserts should be blazingly fast.
+func generatePoint(id int) Point {
+	x := int(rand.Int31() / 2)
+	y := x * 2
+	return Point{id, x, y}
+}
+
+// createJsonPackage takes in an id and creates a json to send to the server
+func createJsonPackage(id int) ([]byte, error) {
+	point := generatePoint(id)
+	mar, err := json.Marshal(point)
+	if err != nil {
+		log.Println(err)
+	}
+	var p Point
+	err = json.Unmarshal(mar, &p)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return json.Marshal(p)
+}
+
+func TestHandler(t *testing.T) {
+	pay, err := createJsonPackage(1)
+	if err != nil {
+		log.Fatal("The Json could not be Marshaled up")
+	}
+
+	req, err := http.NewRequest("POST", "http://127.0.0.1:8080", bytes.NewReader(pay))
+	if err != nil {
+		log.Println("Could not set up the request")
+		log.Fatal(err)
+	}
+	
+	w := httptest.NewRecorder()
+	Handler(w, req, db)
+	if w.Code != 200 {
+		t.Fail()
+	}
 }
